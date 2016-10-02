1e02630edf6352b4beb99405a26343f0d39eb903
Marcus Vinícius <Marcus Vincius>
2016-10-02 11:33:39 -0300

    First commit.

---
+++ Assets/TextController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello World!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
